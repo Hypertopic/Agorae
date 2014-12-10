Hypertopic/Agorae

commit 3db19efb60fcbf85a2339b8f17f238fd1624b692

FIX: Items names should be postfixed with their corpus in topic page.
Fixes #18.

_attachments/js/agorae.js
@@ -808,11 +808,17 @@
       return false;
     };
     function appendItem(idx, item){
-      var el = $('<li><img class="del ctl hide" src="css/blitzer/images/delete.png">'
-                   + '<img class="unlink ctl hide" src="css/blitzer/images/unlink.png">'
-                   + '<span class="editable">' + item.name + '</span></li>')
-                   .attr("id", item.id).attr("corpus", item.corpus);
-      $('ul#item').append(el);
+      var corpus = $.agorae.getCorpus(
+        $.agorae.config.servers[0] + "corpus/" + item.corpus,
+        function(corpus) {
+          var el = $('<li><img class="del ctl hide" src="css/blitzer/images/delete.png">'
+            + '<img class="unlink ctl hide" src="css/blitzer/images/unlink.png">'
+            + '<span class="editable">' + item.name + ' (' + corpus.name[0]
+            + ')</span></li>'
+          ).attr("id", item.id).attr("corpus", item.corpus);
+          $('ul#item').append(el);
+        }
+      );
     };
     function onItemClick(){
       var uri = $.getUri(),

commit 874bcd656497e5ce24f27548e1976fd9611339b1

FIX: The corpus suffix of an item is not a part of its name.
Bug on renaming an item on a topic page,
introduced in 3db19efb60fcbf85a2339b8f17f238fd1624b692.

_attachments/js/agorae.js
@@ -810,7 +810,7 @@
         function(corpus) {
           var el = $('<li><img class="del ctl hide" src="css/blitzer/images/delete.png">'
             + '<img class="unlink ctl hide" src="css/blitzer/images/unlink.png">'
-            + '<span class="editable">' + item.name + ' (' + corpus.name[0]
+            + '<span class="editable">' + item.name + '</span><span> (' + corpus.name[0]
             + ')</span></li>'
           ).attr("id", item.id).attr("corpus", item.corpus);
           $('ul#item').append(el);