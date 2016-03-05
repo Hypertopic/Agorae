Hypertopic/Agorae

commit 7e4ed41800daa0edf1b0ae0a00d695e0e208368c

TEST: Create a viewpoint (and name it).
The UI has been slightly changed to become testable.

couchdb/_attachments/js/agorae.js
@@ -47,7 +47,7 @@
         $.agorae.session.username = $.cookie('session.username');
         $.agorae.session.password = $.cookie('session.password');
         $('span.sign-in').hide().next().show();
-        $('#index-edit-toggle').show();
+        $('#toggle').show();
       }
       $(window).hashchange($.agorae.pagehelper.route).hashchange();
     };
@@ -93,35 +93,32 @@
     };
     this.toggle = function(show, clickon, clickoff){
       if(typeof $.agorae.session.username == "undefined" || show !== true){
-        $('input#index-edit-toggle').hide();
+        $('#toggle').hide();
         return false;
       }
-      $('input#index-edit-toggle').show().iToggle({
-      	easing: 'easeOutExpo',
-      	type: 'radio',
-      	keepLabel: true,
-      	easing: 'easeInExpo',
-      	speed: 300,
-      	onClickOn: function(){
-      	  if(clickon) clickon();
-      		$('.ctl').show();
-      		$('ul.links').addClass('editing').removeClass('links');
-      	},
-      	onClickOff: function(){
-      	  if(clickoff) clickoff();
-      	  $('.ctl').hide();
-      	  $('ul.editing').addClass('links').removeClass('editing');
-      	}
+      $('#toggle').show().toggles({
+        width: 50,
+        height: 20
+      }).on('toggle', function(e, active) {
+        if (active) {
+          if(clickon) clickon();
+          $('.ctl').show();
+          $('ul.links').addClass('editing').removeClass('links');
+        } else {
+          if(clickoff) clickoff();
+          $('.ctl').hide();
+          $('ul.editing').addClass('links').removeClass('editing');
+        }
       });
     };
     this.checkController = function(){
-      if($('#index-edit-toggle').prop('checked'))
+      if ($('#toggle').data('toggles').active)
         $('.ctl').show();
       else
         $('.ctl').hide();
     };
     this.hideController = function(){
-      $('#index-edit-toggle').removeProp('checked').hide().parent().hide();
+      $('#toggle').toggle(false).hide();
       $('.ctl').hide();
     };
     this.navigatorBar = function(obj){
@@ -231,7 +228,7 @@
             $.agorae.session.username = data.name;
             $.agorae.session.password = data.password;
             $('span.sign-in').hide().next().show();
-            $('#index-edit-toggle').show();
+            $('#toggle').show();
             $.agorae.pagehelper.route();
           });
         }
@@ -278,7 +275,6 @@
       else
         corpusUrl = corpus;
       $.agorae.getCorpus(corpusUrl, function(corpus){
-        $.log(corpus);
         var el = $('<li><span>' + corpus.name + '</span></li>').attr("id", corpus.id).attr("uri", corpusUrl);
         $('ul#corpus').append(el);
       });
@@ -335,8 +331,7 @@
       });
     };
     function onCorpusClick(){
-      if(!$('#index-edit-toggle').prop('checked'))
-      {
+      if (!$('#toggle').data('toggles').active) {
         var uri = $(this).parent().attr('uri');
         $.agorae.pagehelper.rewrite(uri);
         return false;
@@ -383,8 +378,7 @@
       });
     };
     function onViewpointClick(){
-      if(!$('#index-edit-toggle').prop('checked'))
-      {
+      if (!$('#toggle').data('toggles').active) {
         var uri = $(this).parent().attr('uri');
         $.agorae.pagehelper.rewrite(uri);
         return false;
@@ -485,8 +479,7 @@
           prefixUrl = $.agorae.getServerUri(itemUri),
           corpusID = $.agorae.getDocumentID(corpusUri),
           itemID = $(this).parent().attr('id');
-      if(!$('#index-edit-toggle').prop('checked'))
-      {
+      if (!$('#toggle').data('toggles').active) {
         var uri = prefixUrl + 'item/' + corpusID + '/' + itemID;
         $.agorae.getItem(uri, null, function(){
           $.agorae.pagehelper.rewrite(uri);
@@ -580,8 +573,7 @@
     };
     this.onTopicClick = function(){
       var uri = $(this).parent().attr('uri');
-      if(!$('#index-edit-toggle').prop('checked'))
-      {
+      if (!$('#toggle').data('toggles').active) {
         $.agorae.pagehelper.rewrite(uri);
         return false;
       }
@@ -822,7 +814,7 @@
           prefixUrl = $.agorae.getServerUri(uri),
           corpusID = $(this).parent().attr('corpus'),
           itemID = $(this).parent().attr('id');
-      if(!$('#index-edit-toggle').prop('checked'))
+      if (!$('#toggle').data('toggles').active)
       {
         var uri = prefixUrl + 'item/' + corpusID + '/' + itemID;
         $.agorae.getItem(uri, null, function(){
@@ -1140,8 +1132,7 @@
     };
     function clickRemoteResource(){
       var url = $(this).attr("rel");
-      if(!$('#index-edit-toggle').prop('checked'))
-      {
+      if (!$('#toggle').data('toggles').active) {
         window.open(url);
         return false;
       }

.rspec
@@ -1,0 +1,2 @@
+--color
+--format documentation

couchdb/_attachments/css/agorae.css
@@ -365,3 +365,6 @@ ul.search-condition button{
 #item-search-result ul li:hover{
 	background-color: #FFB3B3;
 }
+#toggle {
+  float: right;
+}

couchdb/_attachments/css/engage.itoggle.css
@@ -1,75 +1,0 @@
-/* ---------------------------------------------
-	TOGGLE.CSS
------------------------------------------------*/
-
-div#itoggle{
-	background:#FFF;
-	-moz-border-radius:10px;
-	-webkit-border-radius:10px;
-	color:#333;
-	float: right;
-}
-div#itoggle h3{
-	margin-bottom:4px;
-	font-size: 14px;
-	font-weight:bold;
-	}
-div#itoggle p{
-	color:#666;
-	margin-bottom:10px;
-	}
-
-div#itoggle input.iT_checkbox_on{
-	display:block !important;
-	float:left;
-	position:relative !important;
-	top:-43px !important;
-	left:100px !important;
-	margin-bottom:-43px;
-	}
-
-/* -----------------------------
-	This is the important bit
--------------------------------*/
-
-div#itoggle label.ilabel{
-	display:block;
-	font-size:12px;
-	padding-bottom:10px;
-	cursor:pointer;
-	}
-
-div#itoggle label.itoggle,
-div#itoggle label.itoggle span{
-	display: block;
-	width: 76px;
-	height: 22px;
-	margin-bottom: 20px;
-	background: url(blitzer/images/itoggle.png) left bottom no-repeat;
-	cursor:pointer;
-	text-indent:-5000px;
-	}
-div#itoggle label.itoggle.iToff{
-	background-position:right bottom;
-	}
-div#itoggle label.itoggle span{
-	background-position: left top;
-	margin: 0;
-	}
-div#itoggle input.iT_checkbox{
-	position:absolute;
-	top:-9999px;
-	left:-9999px;
-	}
-
-/*
-	If you want the corners to be
-	transparent, add these two lines
-	to the main block above. Only
-	works on webkit and mozilla browsers
-	For more reliable results, make your
-	own png using the supplied .psd
-
-	-moz-border-radius:6px;
-	-webkit-border-radius:6px;
-*/

couchdb/_attachments/css/toggles-light.css
@@ -1,0 +1,33 @@
+.toggle-light .toggle-slide {
+  border-radius: 9999px;
+  box-shadow: 0 0 0 1px #999;
+}
+.toggle-light .toggle-on,
+.toggle-light .toggle-off {
+  font-size: 11px;
+  font-weight: 500;
+}
+.toggle-light .toggle-on,
+.toggle-light .toggle-select .toggle-inner .active {
+  background: #45a31f;
+  box-shadow: inset 2px 2px 6px rgba(0, 0, 0, 0.2);
+  text-shadow: 1px 1px rgba(0, 0, 0, 0.2);
+  color: rgba(255, 255, 255, 0.8);
+}
+.toggle-light .toggle-off,
+.toggle-light .toggle-select .toggle-on {
+  color: rgba(0, 0, 0, 0.6);
+  text-shadow: 0 1px rgba(255, 255, 255, 0.2);
+  background: -webkit-linear-gradient(#cfcfcf, #f5f5f5);
+  background: linear-gradient(#cfcfcf, #f5f5f5);
+}
+.toggle-light .toggle-blob {
+  border-radius: 50px;
+  background: -webkit-linear-gradient(#f5f5f5, #cfcfcf);
+  background: linear-gradient(#f5f5f5, #cfcfcf);
+  box-shadow: 1px 1px 2px #888;
+}
+.toggle-light .toggle-blob:hover {
+  background: -webkit-linear-gradient(#e4e4e4, #f9f9f9);
+  background: linear-gradient(#e4e4e4, #f9f9f9);
+}

couchdb/_attachments/css/toggles.css
@@ -1,0 +1,25 @@
+.toggle-slide {
+  overflow: hidden;
+  cursor: pointer;
+  -webkit-user-select: none;
+     -moz-user-select: none;
+      -ms-user-select: none;
+          user-select: none;
+  direction: ltr;
+  text-align: center;
+}
+div.disabled > .toggle-slide {
+  opacity: 0.7;
+  pointer-events: none;
+}
+.toggle-slide .toggle-on,
+.toggle-slide .toggle-off,
+.toggle-slide .toggle-blob {
+  float: left;
+}
+.toggle-slide .toggle-blob {
+  position: relative;
+  z-index: 99;
+  cursor: hand;
+  cursor: grab;
+}

couchdb/_attachments/dialog/_login.html
@@ -5,11 +5,11 @@
       Identification
     </p>
     <table summary=""><tbody><tr>
-      <th><label>Nom d'utilisateur :</label></th>
-      <td><input type="text" name="name" size="34" value=""></td>
+      <th><label for="login">Nom d'utilisateur :</label></th>
+      <td><input id="login" type="text" name="name" size="34" value=""></td>
     </tr><tr>
-      <th><label>Mot de passe :</label></th>
-      <td><input type="password" name="password" size="34" value=""></td>
+      <th><label for="password">Mot de passe :</label></th>
+      <td><input id="password" type="password" name="password" size="34" value=""></td>
     </tr><tr>
       <th></th>
       <td class="cookie"><label>Rester connecté</label><input type="checkbox" name="cookie" size="34" value="1"></td>

couchdb/_attachments/index.html
@@ -4,13 +4,14 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<link type="text/css" href="css/blitzer/jquery-ui.min.css" rel="stylesheet" />
 	<link type="text/css" href="css/agorae.css" rel="stylesheet" />
-	<link type="text/css" href="css/engage.itoggle.css" rel="stylesheet" />
+	<link type="text/css" href="css/toggles.css" rel="stylesheet" />
+	<link type="text/css" href="css/toggles-light.css" rel="stylesheet" />
 	<link rel="stylesheet" type="text/css" href="./fancybox/jquery.fancybox-1.3.4.css" media="screen" />
 
 	<script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
 	<script type="text/javascript" src="js/jquery-ui.min.js"></script>
 	<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
-	<script type="text/javascript" src="js/engage.itoggle.js"></script>
+	<script type="text/javascript" src="js/toggles.min.js"></script>
 	<script type="text/javascript" src="js/jquery.cookie.js"></script>
 	<script type="text/javascript" src="js/jquery.dialog.js"></script>
 	<script type="text/javascript" src="js/jquery.url.js"></script>
@@ -43,7 +44,7 @@
     <div id="header-buttonbar">
       <span class="item-search"><img border="0" src="css/blitzer/images/search.png"><a id="item-search" href="#"><span>Rechercher par attributs</span></a></span>
       <span><img border="0" src="css/blitzer/images/shortcut.png"><a id="shortcut" href="#"><span>Rechercher par thème</span></a></span>
-      <span class="sign-in"><img border="0" src="css/blitzer/images/sign-in.png"><a id="sign-in"><span>Se connecter</span></a></span>
+      <span class="sign-in"><img border="0" src="css/blitzer/images/sign-in.png"><a id="sign-in" href="#">Se connecter</a></span>
       <span class="sign-out hide"><img border="0" src="css/blitzer/images/sign-out.png"><a id="sign-out"><span>Se déconnecter</span></a></span>
     </div>
   </div>

commit 0ee2aff8b4a8e0c57f5ea080b15009b2d2965fdd

FIX: Browsing on click.
Regression introduced by 7e4ed41800daa0edf1b0ae0a00d695e0e208368c.

couchdb/_attachments/js/agorae.js
@@ -1,4 +1,8 @@
 (function($) {
+  function isToggleOn() {
+    var data = $('#toggle').data('toggles');
+    return (data)? data.active : false;
+  };
   function PageHelper(){
     this.init = function(config){
       //resize main layer size
@@ -112,7 +116,7 @@
       });
     };
     this.checkController = function(){
-      if ($('#toggle').data('toggles').active)
+      if (isToggleOn())
         $('.ctl').show();
       else
         $('.ctl').hide();
@@ -331,7 +335,7 @@
       });
     };
     function onCorpusClick(){
-      if (!$('#toggle').data('toggles').active) {
+      if (!isToggleOn()) {
         var uri = $(this).parent().attr('uri');
         $.agorae.pagehelper.rewrite(uri);
         return false;
@@ -378,7 +382,7 @@
       });
     };
     function onViewpointClick(){
-      if (!$('#toggle').data('toggles').active) {
+      if (!isToggleOn()) {
         var uri = $(this).parent().attr('uri');
         $.agorae.pagehelper.rewrite(uri);
         return false;
@@ -479,7 +483,7 @@
           prefixUrl = $.agorae.getServerUri(itemUri),
           corpusID = $.agorae.getDocumentID(corpusUri),
           itemID = $(this).parent().attr('id');
-      if (!$('#toggle').data('toggles').active) {
+      if (!isToggleOn()) {
         var uri = prefixUrl + 'item/' + corpusID + '/' + itemID;
         $.agorae.getItem(uri, null, function(){
           $.agorae.pagehelper.rewrite(uri);
@@ -573,7 +577,7 @@
     };
     this.onTopicClick = function(){
       var uri = $(this).parent().attr('uri');
-      if (!$('#toggle').data('toggles').active) {
+      if (!isToggleOn()) {
         $.agorae.pagehelper.rewrite(uri);
         return false;
       }
@@ -814,8 +818,7 @@
           prefixUrl = $.agorae.getServerUri(uri),
           corpusID = $(this).parent().attr('corpus'),
           itemID = $(this).parent().attr('id');
-      if (!$('#toggle').data('toggles').active)
-      {
+      if (!isToggleOn()) {
         var uri = prefixUrl + 'item/' + corpusID + '/' + itemID;
         $.agorae.getItem(uri, null, function(){
           $.agorae.pagehelper.rewrite(uri);
@@ -1132,7 +1135,7 @@
     };
     function clickRemoteResource(){
       var url = $(this).attr("rel");
-      if (!$('#toggle').data('toggles').active) {
+      if (!isToggleOn()) {
         window.open(url);
         return false;
       }