907cefdad8e1b422ce2f39d0ff8275ede986debe
Etienne JACQUOT <etiennej70@hotmail.fr>
2016-04-19 00:43:10 +0200

    Test "search an item by theme"

--- spec/features/search_item.rb
+++ spec/features/search_item.rb
@@ -9,4 +9,12 @@ feature 'Search an item' do
     expect(page).to have_content('Tests de recette automatisés')
   end
 
+    scenario 'by theme' do
+    visit $home_page
+    click_on_span 'Rechercher par thème'
+    click_on_link 'Formation de l\'UTT'
+    click_on 'Sélectionner'
+    expect(page).to have_content 'ISI – Informatique et systèmes d\'information'
+  end
+
 end
