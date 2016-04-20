4810774802d43612baa81d74fe3746d4969a06ea
EDOH-KOSSI <amenuve_mokpokpo.edoh_kossi@utt.fr>
2016-04-20 22:04:46 +0200

    Update functions with only needed for test my own scenario

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,12 @@
 require 'capybara/rspec'
-require 'capybara/poltergeist'
+require 'capybara/webkit'
 
 Capybara.run_server = false
-Capybara.default_driver = :poltergeist
-Capybara.app_host = "http://agorae.test.hypertopic.org"
+Capybara.default_driver = :webkit
+Capybara.default_wait_time = 5
+Capybara.app_host ='http://agorae.test.hypertopic.org/#http://agorae.test.hypertopic.org/argos/item/cd48c7fdc83cb4ab1a61f574a631d4ec/5329d9bc4c34fd2ef8ccb896004e0a0a'
 
 $home_page = '/'
-
-def in_dialog()
-  find('#dialog')
-end
 
 def log_in_as(login, password)
   fill_in "Nom d'utilisateur", :with => login
@@ -18,8 +15,18 @@ def log_in_as(login, password)
   expect(page).not_to have_content 'Connectez-vous'
 end
 
+def type(*keys_sequences)
+  keys_sequences.each do |s|
+    find('input[type=textbox]').native.send_keys s
+  end
+end
+
 def toggle_edit()
   find('#toggle').click
+end
+
+def a_string()
+  s = ('a'..'z').to_a.shuffle[0,8].join
 end
 
 def click_plus_sign_next_to(list)
@@ -28,18 +35,4 @@ end
 
 def click_last(list)
   find("##{list} li:last-child .editable").click
-end
-
-def click_on_link(text)
-  find('span', :text => "#{text}").click
-end
-
-def type(*keys_sequences)
-  keys_sequences.each do |s|
-    find('input[type=textbox]').native.send_keys s
-  end
-end
-
-def a_string()
-  s = ('a'..'z').to_a.shuffle[0,8].join
 end
