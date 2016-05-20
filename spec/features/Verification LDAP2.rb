b8b9cac80ecbb0f91bd4e19d6b57c8e2c3d85eb1
Alexandre CREUSOT <alex.creuz@live.fr>
2016-05-20 12:57:39 +0200

    Add files via upload

---
+++ spec/features/Verification LDAP2.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+feature 'Verification des informations via LDAP' do
+
+  scenario 'Verification' do
+      log_in_as 'alice' 'lapinblanc'
+      visit '/'
+      click_on_link 'Etudiant'
+      expect(page).to have_content 'Baptiste BAINIER'
+    end
+end
