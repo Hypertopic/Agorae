97c42c51a17a57801ac8ee81278a2130761d3c54
Jessy Barthelemy <jessy-barthelemy@hotmail.fr>
2016-05-27 10:01:16 +0200

    Update Ajouter un centre d'intérêt à la liste des centres d'intérêts.rb

--- spec/features/Ajouter un centre d'intérêt à la liste des centres d'intérêts.rb
+++ spec/features/Ajouter un centre d'intérêt à la liste des centres d'intérêts.rb
@@ -6,7 +6,7 @@ feature 'Ajouter un thème' do
     visit '/'
     click_on 'Se connecter'
     log_in_as 'alice', 'lapinblanc'
-    click_on 'ISI – Informatique et systèmes d'information'
+    click_on 'ISI – Informatique et systèmes d\'information'
     click_on 'IF05 – Qualité du logiciel'
     toggle_edit()
     click_on '.add.ctl' #ajouter un id à l'image (+)
