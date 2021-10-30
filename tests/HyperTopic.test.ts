d5bede0a1ddf2a021d26594cd98a9b8c925a3be7
swve <bdswve@gmail.com>
2021-10-30 13:19:18 +0200

    use config in test

--- tests/HyperTopic.test.ts
+++ tests/HyperTopic.test.ts
@@ -7,6 +7,6 @@ const agoraeConfig = getAgoraeConfig();
 const Hyper = new HyperTopic([agoraeConfig.argos.url]);
 
 test("Check basic HyperTopic call", async () => {
-  const data = await Hyper.getView(`/item/${agoraeConfig.argos.corpus}/85bb03f5e4930f3b9d1ef9afbfa92421b8e2e23b`);
-  console.log(data);
+  const data = await Hyper.getView(`/corpus/${agoraeConfig.argos.corpus}`);
+  console.log(data[agoraeConfig.argos.corpus]);
 });
